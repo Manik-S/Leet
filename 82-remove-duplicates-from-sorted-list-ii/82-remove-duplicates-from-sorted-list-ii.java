627a48db4bb0e09eaadbb5eb9108cd0650ddf6d4
Manik <97842079+Manik-S@users.noreply.github.com>
2022-06-29 00:59:56 +0530

    Time: 1 ms (80.54%), Space: 44.1 MB (25.41%) - LeetHub

---
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode left=null;
+        ListNode right;
+        ListNode newHead=null;
+        int valu=-1101010;
+        while(head !=null)
+        {
+            if(head.next!=null)
+            {
+               if(head.val==head.next.val && left!=null)
+                {
+                    head=head.next;
+                    valu=head.val;
+                    left.next=null;
+                }
+                else if(head.val==head.next.val && left==null)
+                {
+                    head=head.next;
+                    valu=head.val;
+                }
+                else
+                {
+                    if(left!=null && head.val!=valu)
+                    {
+                        left.next=head;            
+                        left=head;
+                    }
+                    else if(left==null && head.val!=valu)
+                    {
+                        newHead=head;            
+                        left=head;
+                    }
+                    head=head.next;
+                } 
+            }
+            else
+            {
+                    if(left!=null && head.val!=valu)
+                    {
+                        left.next=head;            
+                        left=head;
+                    }
+                    else if(left==null && head.val!=valu)
+                    {
+                        newHead=head;            
+                        left=head;
+                    }
+                    head=head.next;
+            }
+            
+        }
+        return newHead;
+        
+    }
+}
